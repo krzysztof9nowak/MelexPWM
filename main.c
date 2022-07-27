02d3e1231caf33a81cdd617595398f7e0697a0fc
Krzysztof Nowak <krzysztof9nowak@gmail.com>
2022-07-27 22:37:12 +0200

    adc & pwm

--- main.c
+++ main.c
@@ -1,21 +1,19 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-
-#define PWM_PIN (1 << 1)
-
-
 int main (void)
 {
-    DDRB |= PWM_PIN; // set PWM pin as output
+    DDRB |= (1 << PB1); // set PWM pin PB1 as output
     TCCR0A = (1 << COM0B1) | (1 << WGM00) | (1 << WGM01); // correct PWM OCR0A
-    TCCR0B = (1 << CS00); // no prescaling
+    TCCR0B = (1 << CS01); // x8 prescaling
+
+    ADMUX = (1 << MUX0); // ADC1 on PB2
+    ADCSRA = (1 << ADEN) | (1<<ADPS0) | (1<<ADPS1) | (1<<ADPS2); // enable, 128x prescaler
 
     while(1){
-        OCR0A++;
-        OCR0B++;
-
-        _delay_ms(50);
+        ADCSRA |= (1<<ADSC); // start conversion
+        while(ADCSRA & (1<<ADSC)); // wait for result
+        OCR0B = ADC / 4;
     }
 
     return 1;
